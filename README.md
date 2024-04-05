d21c705636a82bce3ad13b631b5dc937238d6dc9
Gleb <naumov.gm@mail.ru>
2024-04-05 09:14:34 +0300

    Update project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # awesome-frontend
-CLI tool for generating frontend side on Node.js
+
+The project is under development.
---
+++ bin/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+/*
+    Author: Gleb Naumov (©) 2024
+    License: MIT
+    Project: https://github.com/glenau/awesome-frontend
+*/
